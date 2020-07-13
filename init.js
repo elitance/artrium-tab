ca9485dfa0ed6544d11f8d006edc6751ea140489
Lucas David Han <ldhan0715@gmail.com>
2020-07-13 22:33:23 +0900

    Update - v0.1

--- init.js
+++ init.js
@@ -6,6 +6,13 @@ function checkMobile() {
     }
 }
 
+function checkBeta() {
+    const betaFeature = JSON.parse(localStorage.getItem("beta"));
+    if (betaFeature === false) {
+        location.replace("https://ldhan0715.github.io/artrium");
+    }
+}
+
 function init() {
     checkMobile();
 }
