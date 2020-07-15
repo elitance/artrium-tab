e2a6b444296a770352a0e478fb244feb0637355d
Lucas David Han <ldhan0715@gmail.com>
2020-07-15 13:21:30 +0900

    Update - v0.2

--- init.js
+++ init.js
@@ -1,8 +1,17 @@
 const mobile = /Android|webOS|iPhone|iPad|iPod|BlackBerry|IEMobile|Opera Mini/i;
+const head = document.querySelector("head");
 
 function checkMobile() {
+    const main = document.querySelector("main");
     if (mobile.test(navigator.userAgent)) {
-        location.replace("blocked.html");
+        const cssLink = document.createElement("link");
+        cssLink.rel = "stylesheet";
+        if (main === null) {
+            cssLink.href = "mobile-main.css";
+        } else {
+            cssLink.href = "mobile-pref.css";
+        }
+        head.appendChild(cssLink);
     }
 }
 
