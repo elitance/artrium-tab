15b53f42d1845ec698f5fbeb5046715fb96ac30a
Lucas David Han <ldhan0715@gmail.com>
2020-07-19 19:31:49 +0900

    Update - v0.85

--- var.js
+++ var.js
@@ -1,6 +1,6 @@
 // Variables of: 'background.js'
 
-Date.prototype.getWeek=function(){var e=new Date(this.getTime());e.setHours(0,0,0,0),e.setDate(e.getDate()+3-(e.getDay()+6)%7);var t=new Date(e.getFullYear(),0,4);return 1+Math.round(((e.getTime()-t.getTime())/864e5-3+(t.getDay()+6)%7)/7)};const body=document.querySelector("body"),date=new Date,week=date.getWeek();let random=Math.floor(9*Math.random());const dateQuery={citys:{0:"australia",1:"uk",2:"france",3:"us",4:"canada",5:"italy",6:"ireland",7:"russia",8:"swiss"},arts:{0:"interior",1:"nature",2:"business",3:"minimal",4:"architecture",5:"sustainability",6:"textures",7:"technology",8:"music"}},accessCode="3CVHz8RtTUrydWpPKmbmNGlOBnZ4zxn6k2YvNn-bjPw";let query;
+Date.prototype.getWeek=function(){var e=new Date(this.getTime());e.setHours(0,0,0,0),e.setDate(e.getDate()+3-(e.getDay()+6)%7);var t=new Date(e.getFullYear(),0,4);return 1+Math.round(((e.getTime()-t.getTime())/864e5-3+(t.getDay()+6)%7)/7)};const body=document.querySelector("body"),date=new Date,week=date.getWeek();let random=Math.floor(10*Math.random());const dateQuery={citys:{0:"australia",1:"uk",2:"france",3:"us",4:"canada",5:"italy",6:"ireland",7:"russia",8:"swiss",9:"landscape"},arts:{0:"interior",1:"nature",2:"business",3:"minimal",4:"architecture",5:"sustainability",6:"textures",7:"technology",8:"music",9:"car"}},accessCode="3CVHz8RtTUrydWpPKmbmNGlOBnZ4zxn6k2YvNn-bjPw";let query;
 
 // Variables of: 'bookmark.js'
 
